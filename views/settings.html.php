5450cbfe2ae4ef1d753634390cd8360d00b87ae5
Giovann <giovann@gilaoss.co.za>
2015-07-22 12:52:06 +0200

    Update settings.html.php
    Varnish notice

--- views/settings.html.php
+++ views/settings.html.php
@@ -1,4 +1,6 @@
 <h1>Icarus Monitoring & Health Check</h1>
 <p>
     Brows to: <a href="<?= $this->status_uri() ?>" ><?= $this->status_uri() ?></a>
+    
+    <strong>Note:</strong> Please disable caching for this page if you using varnish.
 </p>
