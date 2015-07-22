cd79daba64dd4f1afced83106fe5014dcfaa1a52
Giovann <giovann@gilaoss.co.za>
2015-07-22 12:53:07 +0200

    Update settings.html.php

--- views/settings.html.php
+++ views/settings.html.php
@@ -1,6 +1,6 @@
 <h1>Icarus Monitoring & Health Check</h1>
 <p>
     Brows to: <a href="<?= $this->status_uri() ?>" ><?= $this->status_uri() ?></a>
-    
+    <br/><br/>
     <strong>Note:</strong> Please disable caching for this page if you using varnish.
 </p>
