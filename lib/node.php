c293d35e15cbc3f667148a1a90067c37e813608b
RSAWEB <thedevs@rsaweb.co.za>
2015-08-03 22:22:13 +0200

    Networking prefix issue

--- lib/node.php
+++ lib/node.php
@@ -38,7 +38,7 @@
       $hostame = `hostname`;
       $ifaces = $settings["iface"];
       $interface = `ifconfig | grep -A 2 {$ifaces}`;
-      var_dump("ifconfig | grep -A 2 {$ifaces}", system("ifconfig | grep -A 2 {$ifaces}"),$interface);
+      var_dump("ifconfig | grep -A 2 {$ifaces}", system("ifconfig | grep -A 2 venet"),$interface);
       preg_match_all("/addr:(([\d\.]{2,3}){4})/", $interface, $data);
       
       $eth0 = join("::", $data[1]);
